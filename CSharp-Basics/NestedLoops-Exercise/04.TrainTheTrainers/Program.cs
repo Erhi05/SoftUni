ecca84894d40185a7b32ca2401ac9326620b5d24
Erhi <erhanaudi80@abv.bg>
2024-12-27 11:52:43 +0200

    Add Last for the Basics course

---
+++ CSharp-Basics/NestedLoops-Exercise/04.TrainTheTrainers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _04.TrainTheTrainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int juryNumber = int.Parse(Console.ReadLine());
+            string presentationName = Console.ReadLine();
+
+            double averageGradeSum = 0;
+            double totalGradeSum = 0;
+            double averageGradeSumTotal = 0;
+            int presentationCount = 0;
+
+            while (presentationName != "Finish")
+            {
+                double presentationGradeSum = 0;
+                for (int i = 0; i < juryNumber; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    presentationGradeSum += grade;
+                }
+
+                averageGradeSum = presentationGradeSum / juryNumber;
+                Console.WriteLine($"{presentationName} - {averageGradeSum:f2}.");
+
+                totalGradeSum += averageGradeSum;
+                presentationCount++;
+
+                presentationName = Console.ReadLine();
+            }
+
+            averageGradeSumTotal = totalGradeSum / presentationCount;
+            Console.WriteLine($"Student's final assessment is {averageGradeSumTotal:F2}.");
+        }
+    }
+}
