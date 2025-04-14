f64a80aac3a76fb4640b4ccb192f2330b650f12f
Erhi <erhanaudi80@abv.bg>
2025-04-14 21:11:07 +0300

    Add [task/course details]

---
+++ CSharp-Fundamentals/Arrays-Lab/01.DayOfWeek/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.DayOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            int day = int.Parse(Console.ReadLine());
+
+            if (day < 1 || day > 7)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(days[day - 1]);
+            }
+        }
+    }
+}
---
+++ CSharp-Fundamentals/Arrays-Lab/07.EqualArrays/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _07.EqualArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int[] secondArray = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            bool areEqual = true;
+
+            int sum = 0;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] != secondArray[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    areEqual = false;
+                    break;
+                }
+                sum += firstArray[i];
+            }
+
+            if (areEqual)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
