eb8c5e3973179e2eed92962aca1da00b9fe5f80e
Erhi <erhanaudi80@abv.bg>
2025-07-14 22:05:17 +0300

    Add [Objects and Classes]

---
+++ CSharp-Fundamentals/ObjectsAndClasses-Lab/01.RandomizeWords/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _01.RandomizeWords
+{
+    internal class Program
+    {
+        class RandomizeWords
+        {
+            public string Input { get; set; }
+            public List<string> RandomizedInput { get; set; }
+
+            public void Randomize()
+            {
+                RandomizedInput = new List<string>();
+
+                List<string> splitted = Input.Split(" ").ToList();
+                int count = splitted.Count;
+                Random random = new Random();
+
+                for (int i = 0; i < count; i++)
+                {
+                    int index = random.Next(0, splitted.Count);
+                    RandomizedInput.Add(splitted[index]);
+
+                    splitted.RemoveAt(index);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            RandomizeWords randomizeWords = new RandomizeWords();
+
+            randomizeWords.Input = Console.ReadLine();
+            randomizeWords.Randomize();
+
+            foreach (var word in randomizeWords.RandomizedInput)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
